97e50fba7457a4776db8852d69bf36bab5d40fe3
Luís Fernando Vendrame <lvendrame@gmail.com>
2014-06-26 17:50:13 -0300

    Home > Index

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 var express = require('express');
+var swig  = require('swig');
 var app = express();
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
+
+app.get('/', function(req, res){
+    res.render('home/index', {});
+});
+
+app.listen(3000);
